29d28423c2c055ddb8f2576b8ccec761dc4f6f1e
Mark Graham <46523622+MarkGGraham@users.noreply.github.com>
2022-02-14 09:03:14 +0000

    Add files via upload

--- week_3/homework.sql
+++ week_3/homework.sql
@@ -1,18 +1,16 @@
 SET @@dataset_id = 'dtc-de-2022-mg-339910';
 
-select * from trips_data_all.high_vol_partioned limit 1;
-
-select count(*) from trips_data_all.high_vol_tripdata 
+select count(*) from dtc-de-2022-mg-339910.trips_data_all.fh_tripdata;
 
 -- Question 1:
 -- What is count for fhv vehicles data for year 2019
-select count(*) from trips_data_all.high_vol_partioned where pickup_datetime between '2019-01-01' and '2019-12-31';
+select count(*) from dtc-de-2022-mg-339910.trips_data_all.fh_partitioned;
 
 -- Question 2:
 -- How many distinct dispatching_base_num we have in fhv for 2019
 -- Can run a distinct query on the table from question 1
 select count(distinct dispatching_base_num)
-from trips_data_all.high_vol_partioned where pickup_datetime between '2019-01-01' and '2019-12-31';
+from dtc-de-2022-mg-339910.trips_data_all.fh_partitioned where pickup_datetime between '2019-01-01' and '2019-12-31';
 
 -- Question 3:
 -- Best strategy to optimise if query always filter by dropoff_datetime and order by dispatching_base_num
@@ -20,18 +18,18 @@ from trips_data_all.high_vol_partioned where pickup_datetime between '2019-01-01
 -- We need to think what will be the most optimal strategy to improve query performance and reduce cost
 
 --create partition on dropoff_datetime and cluster by dispatching_base_num as follows
-create or replace table dtc-de-2022-mg-339910.trips_data_all.high_vol_tripdata_part_clust_2
-partition by DATE(dropoff_datetime)
+create or replace table dtc-de-2022-mg-339910.trips_data_all.fh_tripdata_partitioned_clustered
+partition by DATE(pickup_datetime)
 cluster by (dispatching_base_num)
-as select * from dtc-de-2022-mg-339910.trips_data_all.high_vol_tripdata;
+as select * from dtc-de-2022-mg-339910.trips_data_all.fh_tripdata;
 
 -- Question 4:
 -- What is the count, estimated and actual data processed for query which counts trip between 2019/01/01 and 2019/03/31 for dispatching_base_num B00987, B02060, B02279
 -- Create a table with optimized clustering and partitioning, and run a count(*). Estimated data processed can be found in top right corner and actual data processed can be found after the query is executed.
 select count(*)
-from dtc-de-2022-mg-339910.trips_data_all.high_vol_tripdata_part_clust_2
+from dtc-de-2022-mg-339910.trips_data_all.fh_tripdata_partitioned_clustered
 where  1=1 
-and dropoff_datetime between '2019-01-01' and '2019-12-31'
+and dropoff_datetime between '2019-01-01' and '2019-03-31'
 and dispatching_base_num in ('B00987', 'B02060', 'B02279')
 
 -- Question 5:
@@ -45,3 +43,4 @@ A composite partition could be created.
 -- Partitioning and clustering also creates extra metadata.
 -- Before query execution this metadata needs to be processed.
 Very little performance improvements would be gained by partitioning or clustered such a small table.
+
